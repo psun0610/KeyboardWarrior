4a2997ff060b6e0c31962ff5af75896772cf46e4
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-11-10 12:18:33 +0900

    accounts/회원관련모든기능

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 
@@ -6,9 +6,34 @@ class CreateUser(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = [
-            "username",
-            "press",
-            "weight",
-            "array",
-            "sound",
+        "username", 
+        "email",
+        "password1",
+        "password2",
+        "press", 
+        "weight", 
+        "array",
+        "sound",
         ]
+        labels = {
+            'username' : '아이디',
+            'email' : '이메일 ',
+            'press' : '키압',
+            'weight': '무게',
+            'array': '배열',
+            'sound': '소리',
+            }
+class CustomUserChangeForm(UserChangeForm):
+  password = None
+
+  class Meta:
+      model = get_user_model()
+      fields = ['username', 'email', 'press', 'weight', 'array', 'sound']
+      labels = {
+            'username' : '아이디',
+            'email' : '이메일 ',
+            'press' : '키압',
+            'weight': '무게',
+            'array': '배열',
+            'sound': '소리',
+            }
