126a03c4f31204c8d70e03e384ea6f3a8b9ff3d8
yoosoonil <y2k7654@gmail.com>
2022-11-11 16:22:28 +0900

    reviews:댓글 비동기 삭제 구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
   path("<int:pk>/update/", views.update, name="update"),
   path("<int:pk>/delete/", views.delete, name="delete"),
   path("<int:pk>/comment_create/", views.comment_create, name="comment_create"),
-  path("<int:review_pk>/comment_delete/<int:comment_pk>", views.comment_delete, name="comment_delete"),
+  path("<int:review_pk>/comment_delete/<int:comment_pk>/delete/", views.comment_delete, name="comment_delete"),
   ]
