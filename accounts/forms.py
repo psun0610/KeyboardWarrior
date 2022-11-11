dd32659315905ce200d4e52e317a4db088ed935d
tenedict <tenedict@gmail.com>
2022-11-11 17:44:43 +0900

    accounts: 회원상세정보

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,37 +6,51 @@ class CreateUser(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = [
-        "username", 
-        "email",
-        "password1",
-        "password2",
-        "press", 
-        "weight", 
-        "array",
-        "sound",
-        "connect",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            "first_name",
+            "last_name",
+            "press",
+            "weight",
+            "array",
+            "sound",
+            "connect",
         ]
         labels = {
-            'username' : '아이디',
-            'email' : '이메일 ',
-            'press' : '키압',
-            'weight': '무게',
-            'array': '배열',
-            'sound': '소리',
-            'connect': '연결',
-            }
+            "username": "아이디",
+            "email": "이메일 ",
+            "press": "키압",
+            "weight": "무게",
+            "array": "배열",
+            "sound": "소리",
+            "connect": "연결",
+        }
+
+
 class CustomUserChangeForm(UserChangeForm):
     password = None
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'press', 'weight', 'array', 'sound','connect',]
+        fields = [
+            "username",
+            "email",
+            "press",
+            "weight",
+            "array",
+            "sound",
+            "connect",
+        ]
         labels = {
-                'username' : '아이디',
-                'email' : '이메일 ',
-                'press' : '키압',
-                'weight': '무게',
-                'array': '배열',
-                'sound': '소리',
-                'connect': '연결',
-                }
+            "username": "아이디",
+            "email": "이메일 ",
+            "press": "키압",
+            "first_name": "이름",
+            "last_name": "성",
+            "weight": "무게",
+            "array": "배열",
+            "sound": "소리",
+            "connect": "연결",
+        }
