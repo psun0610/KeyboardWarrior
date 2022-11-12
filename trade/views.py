502ebb3398568534e0867882489f00d3664ea68c
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-11-13 01:38:51 +0900

    reviews,trade/abusefix

--- trade/views.py
+++ trade/views.py
@@ -4,7 +4,7 @@ from .forms import CreateTrade, CreateComment
 from .models import Trades, Trade_Comment
 from django.http import JsonResponse
 from articles.models import Keyboard
-
+from datetime import date, datetime, timedelta
 # Create your views here.
 def maketable(p):
 	table = [0] * len(p)
@@ -35,6 +35,7 @@ def index(request):
     trades = Trades.objects.all()
     context = {"trades": trades}
     return render(request, "trade/index.html", context)
+
 
 @login_required
 def create(request):
@@ -73,18 +74,23 @@ def update(request, pk):
         }
     return render(request, "trade/update.html", context)
 
+
 def detail(request, pk):
     trade = get_object_or_404(Trades, pk=pk)
     comments = Trade_Comment.objects.filter(trade=pk).order_by("-pk")
     comment_from = CreateComment()
-    for t in comments:
+    for c in comments:
         with open('filtering.txt') as txtfile:
             for word in txtfile.readlines():
                 word = word.strip()
-                ans = KMP(word, t.content)
+                ans = KMP(word, c.content)
                 if ans:
-                    k = int(ans[0])
-                    t.content = len(t.content[k - 1 : len(word)]) * "*" + t.content[len(word):]
+                    for k in ans:
+                        k = int(k)
+                        if k < len(c.content) // 2:
+                            c.content = len(c.content[k - 1 : len(word)]) * "*" + c.content[len(word):]
+                        else:
+                            c.content = c.content[0 : k - 1] + len(c.content[k - 1:]) * "*"
     context = {
         "trade": trade,
         "comment_from": comment_from,
@@ -120,9 +126,12 @@ def trade_comment(request, pk):
                     word = word.strip()
                     ans = KMP(word, c.content)
                     if ans:
-                        k = int(ans[0])
-                        c.content = len(c.content[k - 1 : len(word)]) * "*" + c.content[len(word):]
-                        break
+                        for k in ans:
+                            k = int(k)
+                            if k < len(c.content) // 2:
+                                c.content = len(c.content[k - 1 : len(word)]) * "*" + c.content[len(word):]
+                            else:
+                                c.content = c.content[0 : k - 1] + len(c.content[k - 1:]) * "*"
             comment_list.append(
                 {
                     "user": c.user.username,
@@ -150,6 +159,18 @@ def delete_comment(request, trade_pk, comment_pk):
         user = request.user
         comment_list = []
         for c in comments:
+            with open('filtering.txt') as txtfile:
+                for word in txtfile.readlines():
+                    word = word.strip()
+                    ans = KMP(word, c.content)
+                    if ans:
+                        for k in ans:
+                            k = int(k)
+                            if k < len(c.content) // 2:
+                                c.content = len(c.content[k - 1 : len(word)]) * "*" + c.content[len(word):]
+                            else:
+                                c.content = c.content[0 : k - 1] + len(c.content[k - 1:]) * "*"
+            
             comment_list.append(
                 {
                     "user": c.user.username,
