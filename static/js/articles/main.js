693f0906c5270c8e2ae2bf07f4e5ad3df0067bda
psun0610 <psy010610@naver.com>
2022-11-15 22:59:19 +0900

    js파일 구분, trade/detail.html 댓글 창 크기 및 간격 조절

--- static/js/articles/main.js
+++ static/js/articles/main.js
@@ -43,36 +43,6 @@ window.addEventListener('load', saFunc);
 window.addEventListener('scroll', saFunc);
 
 
-// all.html
-let nav_flag = 0
-document.querySelector('#all-nav-button').addEventListener("click",  function() {
-  if(!nav_flag){
-    const nav_close = document.querySelector('.all-nav-close')
-    nav_close.className = 'all-nav-open'
-    const nav_table = document.querySelector('#nav-table')
-    nav_table.style.display = 'block'
-    nav_flag = 1
-  }
-  else {
-    const nav_close = document.querySelector('.all-nav-open')
-    nav_close.className = 'all-nav-close'
-    const nav_table = document.querySelector('#nav-table')
-    nav_table.style.display = 'none'
-    nav_flag = 0
-  }
-})
-
-const ad_card = document.querySelectorAll('.ad-card')
-
-ad_card.forEach((card, index) => {
-  card.addEventListener('mouseover', function() {
-    this.querySelector('.ad-card-context').className = 'ad-card-context-hover'
-  })
-  card.addEventListener('mouseout', function() {
-    this.querySelector('.ad-card-context-hover').className = 'ad-card-context'
-  })
-})
-
 new Swiper('#slide', {
   auto_play: true,
   slidesPerView: 1, // 동시에 보여줄 슬라이드 갯수
