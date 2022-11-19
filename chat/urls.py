583c001f09b49494417e77a7a63e1d9b11270ef7
caretim <karesin72041@gmail.com>
2022-11-20 02:38:03 +0900

    chat/ulr설정

--- chat/urls.py
+++ chat/urls.py
@@ -8,5 +8,6 @@ app_name = "chat"
 urlpatterns = [
     path("", views.index, name="index"),
     # path("<str:trade_pk>/", views.room, name="room"),
-    path("<str:room_name>/<int:user__pk>", views.room, name="room"),
+    path("<str:room_name>/", views.room, name="room"),
+    path("<int:trade_pk>/", views.find_room, name="find_room"),
 ]
