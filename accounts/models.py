6359e99e237c892a3284d2dc3a07aab105bfa3ad
psun0610 <psy010610@naver.com>
2022-11-19 22:46:51 +0900

    chat add

--- accounts/models.py
+++ accounts/models.py
@@ -60,21 +60,21 @@ class User(AbstractUser):
     notice = models.IntegerField(default=0)
 
 
-class Room(models.Model):
-    send_user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    reception_user = models.ForeignKey(
-        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="받는사람"
-    )
-    count = models.IntegerField(default=0)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    trade = models.ForeignKey(Trades, on_delete=models.CASCADE)
+# class Room(models.Model):
+#     send_user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     reception_user = models.ForeignKey(
+#         AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="받는사람"
+#     )
+#     count = models.IntegerField(default=0)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+#     trade = models.ForeignKey(Trades, on_delete=models.CASCADE)
 
 
-class Message(models.Model):
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.CharField(max_length=1000)
-    created_at = models.DateTimeField(auto_now_add=True)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+# class Message(models.Model):
+#     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     content = models.CharField(max_length=1000)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
 class signal(models.Model):
