8ee2fd21db00146107ebfcbd967cd7f027c621b9
caretim <karesin72041@gmail.com>
2022-11-21 11:04:28 +0900

    로그인픽스

--- accounts/views.py
+++ accounts/views.py
@@ -235,7 +235,7 @@ state_token = secrets.token_urlsafe(16)
 def naver_request(request):
     naver_api = "https://nid.naver.com/oauth2.0/authorize?response_type=code"
     client_id = "2HYYDV4He0l2acImyKCo"  # 배포시 보안적용 해야함
-    redirect_uri = "http://keyboardwarriorbean-env.eba-uzmimep3.ap-northeast-2.elasticbeanstalk.com/accounts/naver/login/callback"
+    redirect_uri = "http://keyboardwarriorbean-env.eba-uzmimep3.ap-northeast-2.elasticbeanstalk.com/accounts/login/naver/callback"
     state_token = secrets.token_urlsafe(16)
     return redirect(
         f"{naver_api}&client_id={client_id}&redirect_uri={redirect_uri}&state={state_token}"
@@ -249,7 +249,7 @@ def naver_callback(request):
         "client_secret": "t1OyNantHN",
         "code": request.GET.get("code"),
         "state": request.GET.get("state"),
-        "redirect_uri": "http://keyboardwarriorbean-env.eba-uzmimep3.ap-northeast-2.elasticbeanstalk.com/accounts/naver/login/callback",
+        "redirect_uri": "http://keyboardwarriorbean-env.eba-uzmimep3.ap-northeast-2.elasticbeanstalk.com/accounts/login/naver/callback",
     }
     naver_token_request_url = "https://nid.naver.com/oauth2.0/token"
     access_token = requests.post(naver_token_request_url, data=data).json()[
